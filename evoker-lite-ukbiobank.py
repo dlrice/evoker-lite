27b2e7119b3d5297e628b098b0ab630db1f668f8
Daniel Rice <Daniel.L.Rice@gmail.com>
2017-06-05 17:48:15 +0100

    Adding plotting of ellipses to UKBB.

--- evoker-lite-ukbiobank.py
+++ evoker-lite-ukbiobank.py
@@ -1,14 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from math import ceil
-from struct import unpack
+from struct import unpack, pack
 import seaborn as sns
 import re
 import os
 import shutil
+from collections import Counter
 
-
-class Variants(object):
+class Variants:
     
     def __init__(self, file_path):
         self.file_path = file_path
@@ -52,7 +52,7 @@ class Variants(object):
         return '\n'.join([str(variant) for variant in self.variants])
 
 
-class Variant(object):
+class Variant:
     
     def __init__(self, chrom, name, pos, coord, A1, A2):
         self.chrom = chrom
@@ -74,7 +74,7 @@ class Variant(object):
     def __str__(self):
         return '{}:{} {}'.format(self.chrom, self.pos, self.name)
 
-class Samples(object):
+class Samples:
     
     def __init__(self, file_path):
         self.file_path = file_path
@@ -105,14 +105,14 @@ class Samples(object):
         return self.__samples.index(sample_id)
 
 
-class Genotypes(object):
+class Genotypes:
     
     def __init__(self, file_path, n_samples, n_variants):
         self.file_path = file_path
         self.n_samples = n_samples
         self.n_variants = n_variants
         self.HEADER_MAGIC = (108, 27, 1)
-#         self.check_file()
+        self.check_file()
         
         
     def check_file(self):
@@ -124,7 +124,6 @@ class Genotypes(object):
     def get_offset(self, variant_index):
         offset = len(self.HEADER_MAGIC)
         offset += ceil(self.n_samples / 4.) * variant_index
-#         offset = ceil(self.n_samples / 4.) * variant_index
         return int(offset)
     
     def get_genotypes(self, variant_index):
@@ -141,7 +140,7 @@ class Genotypes(object):
         return np.array(genotypes[:self.n_samples])
 
 
-class BinaryIntensity(object):
+class BinaryIntensity:
     
     def __init__(self, file_path, variants, samples):
         self.file_path = file_path
@@ -162,8 +161,6 @@ class BinaryIntensity(object):
         assert  magic == self.HEADER_MAGIC
 
     def get_offset(self, variant_index):
-#         offset = len(self.HEADER_MAGIC)
-#         offset += 8 * variant_index * self.samples.get_n_samples()
         offset = 8 * variant_index * self.samples.get_n_samples()
         return offset
     
@@ -193,16 +190,149 @@ class BinaryIntensity(object):
             _bytes = self.f.read(8)
             xy.append(unpack('ff', _bytes))
         return xy
+
+
+class Batches:
     
-class EvokerLite(object):
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.load_batches()
+        
+    def load_batches(self):
+        with open(self.file_path) as f:
+            self.batches = {x.strip(): line_number for line_number, x in enumerate(f.readlines())}
+           
+    def get_index(self, batch_name):
+        return self.batches[batch_name]
     
-    def __init__(self, bfile_path=None, bnt_path=None, int_path= None, fam_path=None, bim_path=None, bed_path=None, exclude_list_path=None):
+    def get_n_batches(self):
+        return len(self.batches)
+
+
+class SNP_Posterior:
+    
+    def __init__(self, file_path, n_batches):
+        self.file_path = file_path
+        self.n_batches = n_batches
+        self.open_file()
+        self.BYTES_PER_BATCH = 4 * 33
+        self.CALLS = ('11', '01', '00') # = ('BB', 'AB', 'AA')
+        
+    def open_file(self):
+        self.f = open(self.file_path, 'rb')
+        
+    def get_offset(self, variant_index, batch_index):
+        """
+        Affymetrix provide 33 parameters per SNP to describe the properties of the genotype calling
+        ellipses for a batch (3,498 per SNP). Each parameter is represented by a 4-byte float. The 
+        set of values for each SNP are ordered consecutively by batch (as given in the batch file).
+        This is analagous to a matrix with rows=SNPs and columns=batch_parameters. The order of the 
+        SNPs and batches are given by the BIM and Batch files.
+        """
+        offset = self.BYTES_PER_BATCH * (variant_index * self.n_batches + batch_index)
+        return offset
+    
+    def get_ellipses_parameters(self, variant_index, batch_index):
+        """
+        The 33 parameters are arranged in four groups. There are 7 parameters for each genotype cluster:
+        BB, AB, AA and a fourth set of 12 parameters (CV) from Affymetrix's algorithm which can be ignored 
+        for most analyses.
+
+        The 7 parameters per cluster are five parameters (mu,sigma) to completely describe the ellipse and
+        two counts of observations (NObsVar can be ignored for most analyses):
+
+            1. mu_X
+            2. sigma_00
+            3. NObsMean
+            4. NObsVar
+            5. mu_Y
+            6. sigma_11
+            7. sigma_off-diagonal(01,10)
+
+        Missing values are represented using the IEEE 754 NaN.
+        """
+        offset = self.get_offset(variant_index, batch_index)
+        self.f.seek(offset)
+        _bytes = self.f.read(self.BYTES_PER_BATCH)
+        X = unpack('f'*int(self.BYTES_PER_BATCH/4), _bytes) # 'f' is IEEE 754 binary32 or 4 bytes (=4*8=32)
+        X = np.array(X, dtype='float')
+        # We only need 21 of the parameters as the spec states the other 12 can be ignored
+        X = X[:21].reshape(3, -1)
+        P = {}
+        for parameters, call in zip(X, self.CALLS):
+            mu_x, sigma_00, n_obs_mean, _, mu_y, sigma_11, sigma_01_10 = parameters
+            P[call] = {
+                'mean': np.array([mu_x, mu_y]),
+                'covariance': np.array([[sigma_00, sigma_01_10], [sigma_01_10, sigma_11]]),
+            }
+
+        return P
+    
+    
+    def get_all_ellipse_points(self, variant_index, batch_index):
+        parameters = self.get_ellipses_parameters(variant_index, batch_index)
+        return {call: self.get_ellipse_points(**ps) for call, ps in parameters.items()}
+
+        
+    def get_ellipse_points(self, mean, covariance):
+        """
+        Based on tutorial at:
+            http://www.visiondummy.com/2014/04/draw-error-ellipse-representing-covariance-matrix/
+        """
+        eigenvalues, eigenvectors = np.linalg.eig(covariance)
+
+        # Get the index of the largest eigenvector
+        largest_eigenvector_index = eigenvalues.argmax()
+        largest_eigenvalue = eigenvalues[largest_eigenvector_index]
+        largest_eigenvector = eigenvectors[:,largest_eigenvector_index]
+
+        # Get the smallest eigenvector and eigenvalue
+        smallest_eigenvector_index = (largest_eigenvector_index + 1) % 2
+        smallest_eigenvalue = eigenvalues[smallest_eigenvector_index]
+        smallest_eigenvector = eigenvectors[:,smallest_eigenvector_index]
+
+        # Calculate the angle between the x-axis and the largest eigenvector
+        angle = np.arctan2(largest_eigenvector[1], largest_eigenvector[0])
+
+        # This angle is between -pi and pi.
+        # Let's shift it such that the angle is between 0 and 2pi
+        if angle < 0:
+            angle += 2*np.pi
+
+        # Get the 95% confidence interval error ellipse
+        chisquare_val = 2.4477
+        theta_grid = np.linspace(0,2*np.pi)
+        phi = angle
+        X0=mean[0]
+        Y0=mean[1]
+        a=chisquare_val*np.sqrt(largest_eigenvalue)
+        b=chisquare_val*np.sqrt(smallest_eigenvalue)
+
+        # the ellipse in x and y coordinates 
+        ellipse_x_r  = a*np.cos(theta_grid)
+        ellipse_y_r  = b*np.sin(theta_grid)
+
+        # Define a rotation matrix
+        R = np.array([[np.cos(phi), np.sin(phi)],[-np.sin(phi), np.cos(phi)]])
+
+        # let's rotate the ellipse to some angle phi
+        r_ellipse = np.dot(np.array([ellipse_x_r, ellipse_y_r]).T, R)
+        return r_ellipse + np.array([X0, Y0])
+    
+class EvokerLite:
+    
+    def __init__(self, bfile_path=None, bnt_path=None, int_path= None, fam_path=None,
+                 bim_path=None, bed_path=None, batch_path=None, snp_posterior_path=None,
+                 exclude_list_path=None):
         self.samples = Samples(fam_path or (bfile_path + '.fam'))
         self.variants = Variants(bim_path or (bfile_path + '.bim'))
         self.genotypes = Genotypes(bed_path or (bfile_path + '.bed'),
                                    self.samples.get_n_samples(),
                                    self.variants.get_n_variants(),
                                   )
+        self.batches = Batches(batch_path or (bfile_path + '.batch'))
+        self.snp_posterior = SNP_Posterior(file_path=snp_posterior_path or (bfile_path + '.snp_posterior'),
+                                          n_batches=self.batches.get_n_batches())
         
         if bnt_path:
             self.intensities = BinaryIntensity(bnt_path, self.variants, self.samples)
@@ -227,7 +357,7 @@ class EvokerLite(object):
             {'code':'00', 'name':'homozygous A1', 'color':'blue', 'label': '{A1}{A1}'.format(A1=A1,A2=A2)},
             {'code':'10', 'name':'heterozygous', 'color':'limegreen', 'label': '{A1}{A2}|{A2}{A1}'.format(A1=A1,A2=A2)},
             {'code':'11', 'name':'homozygous A2', 'color':'red', 'label': '{A2}{A2}'.format(A1=A1,A2=A2)},
-            {'code':'01', 'name':'missing', 'color':'lightgrey', 'alpha':0.9, 'lw':0.75, 'label': 'Missing'},
+            {'code':'01', 'name':'missing', 'color':'lightgrey', 'alpha':0.9, 'lw':0.75, 'label': 'Missing|Unclassified'},
         )
         genotypes = self.genotypes.get_genotypes(variant_index)
         xy = self.intensities.get_intensities_for_variant(variant_index)
@@ -236,7 +366,8 @@ class EvokerLite(object):
             batch_indices = batches == batch
             genotypes = genotypes[batch_indices]
             xy = xy[batch_indices]
-            
+        batch_index = self.batches.get_index(batch)
+        ellipses = self.snp_posterior.get_all_ellipse_points(variant_index, batch_index)
         fig, ax = plt.subplots(figsize=(10, 10))
         for m in GENOTYPE_MAPPING:
             code = m['code']
@@ -249,10 +380,9 @@ class EvokerLite(object):
                        edgecolor='black',
                        label=m['label']
                       )
-#         xymin = min(-0.1, xy.min())
-#         xymax = 1.1 * xy.max()
-#         ax.set_xlim([xymin,xymax])
-#         ax.set_ylim([xymin,xymax])
+            if code in ellipses:
+                ellipse_points = ellipses[code]
+                plt.plot(ellipse_points[:,0], ellipse_points[:,1], color='black')
         ax.spines['top'].set_visible(False)
         ax.spines['right'].set_visible(False)
         ax.yaxis.set_ticks_position('left')
@@ -276,7 +406,16 @@ class EvokerLite(object):
 
 
 if __name__ == '__main__':
-    bfile_path = '/lustre/scratch115/realdata/mdt0/teams/barrett/users/dr9/UKBioBankEvoker/V2_QCed.export.UKBiLEVEAX_b1-b11.Batch_b001_b095.chr22'
-    bnt_path = '/lustre/scratch115/realdata/mdt0/teams/barrett/users/dr9/UKBioBankEvoker/V2_QCed.export.intensity.chr22.bin'
-    bb = EvokerLite(bfile_path, bnt_path=bnt_path)
-    bb.plot_save_all_batches('rs62224618', '/lustre/scratch115/realdata/mdt0/teams/barrett/users/dr9/UKBioBankEvoker/plots')
+    bfile_path = '/Users/dr9/ukbb/chr22/export.22'
+    fam_path = '/Users/dr9/ukbb/chr22/export.fam'
+    bnt_path = '/Users/dr9/ukbb/chr22/export.22.bnt'
+    bb = EvokerLite(bfile_path, bnt_path=bnt_path, fam_path=fam_path)
+    
+    variant = 'rs5771002'
+    batch = 'Batch_b058'
+
+    variant_index = bb.variants.get_index(variant)
+    bb_genotypes = bb.genotypes.get_genotypes(variant_index)
+    fig = bb.plot_intensities(variant, batch)
+    filename = '{}_{}.png'.format(variant, batch)
+    fig.savefig(filename)
